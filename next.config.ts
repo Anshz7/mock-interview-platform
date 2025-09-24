70954eaabb69a0b241b0c45416f8e562cb5cfafd
Ansh <anshshrivastava070204@gmail.com>
2025-09-24 20:17:48 +0530

    setup complete with auth page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: "C:\\Users\\Ansh Shrivastava\\Desktop\\mock_interview_platform"
+  }
 };
 
 export default nextConfig;
